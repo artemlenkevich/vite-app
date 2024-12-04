97aa22ad2f7ec0d7d360c85b5b3319531f1b0d6e
Artem Lenkevich <ar.lenkevich@gmail.com>
2024-12-04 22:00:18 +0300

    init

---
+++ src/components/Component2.tsx
@@ -0,0 +1,12 @@
+// document.querySelector('body')!.style.backgroundColor = 'red'
+
+// @ts-ignore
+// window.a = 5
+function mylog() {
+  console.log('hello');
+}
+mylog()
+
+export const Component2 = () => {
+  return <div>Component2</div>;
+};
---
+++ src/components/index.ts
@@ -0,0 +1,3 @@
+export { Component1 } from './Component1';
+export { Component2 } from './Component2';
+export { Component3 } from './Component3';
